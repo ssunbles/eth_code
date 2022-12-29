2f4440d961c72bbff29cf52f8833bc685cc85f49
ssunbles <106204616+ssunbles@users.noreply.github.com>
2022-12-30 01:18:35 +0300

    Add files via upload

---
+++ contracts/HelloWorld.sol
@@ -0,0 +1,13 @@
+pragma solidity >= 0.5.0;
+
+contract HelloWorld {
+    string public payload;
+
+    function setPayload(string memory content) public {
+        payload = content;
+    }
+
+    function sayHello() public pure returns (string memory) {
+        return 'Hello World!';
+    }
+}
